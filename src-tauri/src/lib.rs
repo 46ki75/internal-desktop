0edb54031f326adbebbea4da3a9458fa09855f95
Ikuma Yamashita <me@ikuma.cloud>
2026-01-16 01:58:50 +0900

    feat: add tauri-plugin-single-instance for single instance application support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -36,6 +36,7 @@ async fn close_command_palette(app: tauri::AppHandle) -> bool {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
